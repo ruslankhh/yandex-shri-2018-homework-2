a6e7349276e3caef7eb209b8d7f6b0eefdf08edc
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-03-11 00:57:36 +0300

    feat: Door2 done

--- public/js/doorOthers.js
+++ public/js/doorOthers.js
@@ -91,21 +91,22 @@ function Door1(number, onUnlock) {
         b.addEventListener('pointerover', _onButtonPointerDown.bind(this));
         b.addEventListener('pointerup', _onButtonPointerUp.bind(this));
         b.addEventListener('pointercancel', _onButtonPointerUp.bind(this));
+        b.addEventListener('pointerleave', _onButtonPointerLeave.bind(this));
     }.bind(this));
 
     this.popup.addEventListener('pointerup', _onButtonPointerUp.bind(this));
 
     function _onButtonPointerDown(e) {
-        if (currentInputIndex < 6) {
+        if (currentInputIndex < 6 && inputs[currentInputIndex] !== undefined) {
             e.target.classList.add('door-riddle__button_pressed');
-        }
 
-        if (currentButton !== e.target) {
-            var input = inputs[currentInputIndex];
-    
-            currentButton = e.target;
-            input.innerHTML = input.innerHTML + e.target.innerHTML;
-            currentInputIndex = currentInputIndex + 1;
+            if (currentButton !== e.target) {
+                var input = inputs[currentInputIndex];
+        
+                currentButton = e.target;
+                input.innerHTML = input.innerHTML + e.target.innerHTML;
+                currentInputIndex = currentInputIndex + 1;
+            }
         }
     }
 
@@ -119,6 +120,10 @@ function Door1(number, onUnlock) {
         inputs.forEach(function(i) {
             i.innerHTML = '';
         });
+    }
+
+    function _onButtonPointerLeave(e) {
+        currentButton = null;
     }
 
     /**
@@ -170,12 +175,97 @@ Door1.prototype.constructor = DoorBase;
 function Door2(number, onUnlock) {
     DoorBase.apply(this, arguments);
 
-    // ==== Напишите свой код для открытия третей двери здесь ====
-    // Для примера дверь откроется просто по клику на неё
-    this.popup.addEventListener('click', function() {
-        this.unlock();
+    // ==== Напишите свой код для открытия второй двери здесь ====
+    var buttons = [
+        this.popup.querySelector('.door-riddle__button_0'),
+        this.popup.querySelector('.door-riddle__button_1')
+    ];
+    var inputs = [
+        this.popup.querySelector('.door-riddle__input_0'),
+        this.popup.querySelector('.door-riddle__input_1'),
+        this.popup.querySelector('.door-riddle__input_2'),
+        this.popup.querySelector('.door-riddle__input_3'),
+        this.popup.querySelector('.door-riddle__input_4'),
+        this.popup.querySelector('.door-riddle__input_5'),
+    ];
+    var currentButton = null;
+    var currentInputIndex = 0;
+
+    buttons.forEach(function(b) {
+        b.addEventListener('pointerdown', _onButtonPointerDown.bind(this));
+        b.addEventListener('pointerover', _onButtonPointerDown.bind(this));
+        b.addEventListener('pointerup', _onButtonPointerUp.bind(this));
+        b.addEventListener('pointercancel', _onButtonPointerUp.bind(this));
+        b.addEventListener('pointerleave', _onButtonPointerLeave.bind(this));
     }.bind(this));
-    // ==== END Напишите свой код для открытия третей двери здесь ====
+
+    this.popup.addEventListener('pointerup', _onButtonPointerUp.bind(this));
+
+    function _onButtonPointerDown(e) {
+        if (currentInputIndex < 6 && inputs[currentInputIndex] !== undefined) {
+            e.target.classList.add('door-riddle__button_pressed');
+
+            if (currentButton !== e.target) {
+                var input = inputs[currentInputIndex];
+        
+                currentButton = e.target;
+                input.innerHTML = input.innerHTML + e.target.innerHTML;
+                currentInputIndex = currentInputIndex + 1;
+            }
+        }
+    }
+
+    function _onButtonPointerUp(e) {
+        buttons.forEach(function(b) {
+            b.classList.remove('door-riddle__button_pressed');
+        });
+        currentButton = null;
+        currentInputIndex = 0;
+        checkCondition.apply(this);
+        inputs.forEach(function(i) {
+            i.innerHTML = '';
+        });
+    }
+
+    function _onButtonPointerLeave(e) {
+        currentButton = null;
+    }
+
+    /**
+     * Проверяем, можно ли теперь открыть дверь
+     */
+    function checkCondition() {
+        var answer = inputs.reduce(function(str, i) {
+            return str + i.innerHTML;
+        }, '');
+
+        var isOpened =
+            answer.length === 6 &&
+            isPrime(parseInt(answer, 2));
+
+        // Если набранное число шестизначное и простое, то откроем эту дверь
+        if (isOpened) {
+            this.unlock();
+        }
+    }
+
+    function isPrime(number) {
+        var result = true;
+
+        if (number < 2) {
+            return false;
+        }
+
+        for (var i = 2; i < Math.sqrt(number); i++) {
+            if (number % i === 0) {
+                result = false;
+            }
+        }
+
+        return result;
+    }
+    
+    // ==== END Напишите свой код для открытия второй двери здесь ====
 }
 Door2.prototype = Object.create(DoorBase.prototype);
 Door2.prototype.constructor = DoorBase;
